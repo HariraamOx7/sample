82b6a4f4d595505ecaca80c263d9a8926d9ac915
B HARIRAAM <hariraam341@gmail.com>
2026-07-22 16:38:14 +0530

    Update and rename sampleprogarm.c to sampleprogarm.java

--- sampleprogarm.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-  int a=10;
-  int b=2;
-  printf("%d + %d :",a,b,a+b);
-  return 0;
-}
---
+++ sampleprogarm.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        System.out.printf("%d + %d : %d%n", a, b, (a + b));
+    }
+}
